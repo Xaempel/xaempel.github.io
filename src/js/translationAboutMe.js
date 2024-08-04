099dee883487fd6e3bd7b1640c23af0f39415da2
Xaempel <xaempelfeedback@gmail.com>
2024-08-04 11:52:26 +0200

    <Feature> -- Added my programming languages informations

--- src/js/translationAboutMe.js
+++ src/js/translationAboutMe.js
@@ -1,17 +1,21 @@
 let getElem = (id) => document.getElementById(id);
 
 function translateAboutMeENG() {
-  getElem("TitleofAboutMeSite").innerText = "About me";
-  getElem("MySkills").innerText = "My Skills";
-  getElem("GeneralSkills").innerText = "General Skills";
-  getElem("ProgramingLanguages").innerText = "My Programing Languages";
-  getElem("MyProjects").innerText = "My Projects";
-  getElem("MyNextPlans").innerText = "My Next Plans";
+  getElem('TitleofAboutMeSite').innerText = 'About me';
+  getElem('MySkills').innerText = 'My Skills';
+  getElem('ProgramingLanguages').innerText = 'My Programing Languages';
+  getElem('MyProjects').innerText = 'My Projects';
+  getElem('MyNextPlans').innerText = 'My Next Plans';
+  getElem('CppPoint').innerText = 'C++ in beginning middle level';
+  getElem('HtmlPoint').innerText = 'Html in basic level';
+  getElem('CssPoint').innerText = 'Css in basic level';
+  getElem('JsPoint').innerText = 'Js in basic level';
+  getElem('PythonPoint').innerText = 'Python in basic level';
 }
 
 function translateAboutMe() {
   switch (selectedLanguages) {
-    case "ENG":
+    case 'ENG':
       translateAboutMeENG();
       break;
   }
